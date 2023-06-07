05ea30b49ee659179f971e23d98b027573fc5418
Andrey "Zed" Zaikin <andrey.zaikin@every-pay.com>
2023-06-07 20:12:04 +0300

    fix place worker

--- Source/PlaceWorker_BedConnector.cs
+++ Source/PlaceWorker_BedConnector.cs
@@ -8,13 +8,15 @@ public class PlaceWorker_BedConnector: PlaceWorker {
     public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map,
             Thing thingToIgnore = null, Thing thing = null) {
 
-        Building edifice = loc.GetEdifice( map );
-        if( !(edifice is Building_Bed || edifice is Building_Enterable) ){
-            return "Must be placed over a bed or an enterable building.";
-        }
+        // LoftBed is not an Edifice!
 
+        bool wasBed = false;
         foreach (Thing thingHere in map.thingGrid.ThingsListAtFast(loc)){
             if( thingHere == thingToIgnore ) continue;
+
+            if( thingHere is Building_Bed || thingHere is Building_Enterable ){
+                wasBed = true;
+            }
 
             if( thingHere.def.defName.Contains("BedConnector") || thingHere.def.defName == "VNPE_ConnectedBed" ){
                 return "IdenticalThingExists".Translate();
@@ -26,6 +28,10 @@ public class PlaceWorker_BedConnector: PlaceWorker {
                 }
                 return new AcceptanceReport("IdenticalThingExists".Translate());
             }
+        }
+
+        if( !wasBed ){
+            return "Must be placed over a bed or an enterable building.";
         }
 
         return true;
